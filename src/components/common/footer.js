af47e07546c1feb5304dae896cb0d06c8bbed3b4
Akhsay Hatkar <akshayhatkar99@gmail.com>
2022-10-25 17:05:34 +0530

    icon color changes

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -5,14 +5,14 @@ const AppFooter = () => {
     <div className='container-fluid'>
     <div className='footer'>
     <div className='logo'> 
-    <i className="fas fa-bolt"></i>
-    <a href="http://google.com">shahupuri yuvak mandal </a><i className="fas fa-bolt"></i>
+    <i className='fa fa-flag' style={{color:"#ff4c00", marginRight: "6px"}}></i>
+     shahupuri yuvak mandal  <i className='fa fa-flag' style={{color:"#ff4c00"}}></i>
     </div>
     <ul className='socials '>
-        <li> <a href='https://www.facebook.com/shahupuriyuvak.mandal/'><i className="fab fa-facebook-f"></i></a> </li>
-        <li> <a href='https://www.twitter.com'><i className="fab fa-twitter"></i></a> </li>
-        <li> <a href='https://www.whatsapp.com'><i className="fab fa-whatsapp"></i></a> </li>
-        <li> <a href='https://www.instagram.com/shahupuri_yuvak_mandal_sym/?hl=en'><i className="fab fa-instagram"></i></a> </li>
+        <li> <a href='https://www.facebook.com/shahupuriyuvak.mandal/'><i className="fab fa-facebook-f" style={{color:"#1877f2"}}></i></a> </li>
+        <li> <a href='https://www.twitter.com'><i className="fab fa-twitter" style={{color:"#1da1f2"}}></i></a> </li>
+        <li> <a href='https://www.whatsapp.com'><i className="fab fa-whatsapp" style={{color:"#24a01b"}}></i></a> </li>
+        <li> <a href='https://www.instagram.com/shahupuri_yuvak_mandal_sym/?hl=en'><i className="fab fa-instagram" style={{color:"#f10c13"}}></i></a> </li>
        
     </ul>
    
--- src/components/home/about.js
+++ src/components/home/about.js
@@ -6,7 +6,7 @@ const items = [
     {
        key : '1',
       //  icon: <i className="fas fa-chart-pie"></i>,
-      icon: <i className="fa fa-trophy" aria-hidden="true"></i>,
+      icon: <i className="fa fa-trophy" aria-hidden="true" style={{color: "gold"}}></i>,
        title :'तांत्रिक देखावा ',
        content :'४० वर्ष परितोषिकांची अखंडित परंपरा कायम.....'
 
@@ -14,7 +14,7 @@ const items = [
     {
         key : '2',
         // icon: <i className="fas fa-desktop"></i>,
-        icon: <i className="fa fa-trophy" aria-hidden="true"></i>,
+        icon: <i className="fa fa-trophy" aria-hidden="true" style={{color: "gold"}}></i>,
         title :'गणपती विसर्जन मिरवणूक',
         content :'अखंडित परंपरा कायम......'
  
@@ -22,7 +22,7 @@ const items = [
      {
         key : '3',
         // icon: <i className="fas fa-database"></i>,
-        icon: <i className="fa fa-trophy" aria-hidden="true"></i>,
+        icon: <i className="fa fa-trophy" aria-hidden="true" style={{color: "gold"}}></i>,
         title :'Shahupuri Yuvak Mandal ',
         content :'... येणाऱ्या "गणराया अवॉर्ड" मधे शाहूपुरी युवक मंडळ…”'
  
